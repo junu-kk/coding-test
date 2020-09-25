f959670e61ab553643a29e2d6cb52da074093fa5
KangJunewoo <brianjune.dev@gmail.com>
2020-09-25 23:46:55 +0900

    [ADD&DOCS] 피보나치 3방식 구현 & 리드미 정리

---
+++ 6-DP/피보나치.py
@@ -0,0 +1,31 @@
+# 피보나치는 쭈루룩 더하는거였지.
+# 1 1 2 3 5 ...
+
+def fibo_slow(x): # 재귀재귀재귀재귀 새로계산 새로계산 -> 시간 매우 오래 걸림.
+    if x == 1 or x == 2:
+        return 1
+    return fibo_slow(x-1) + fibo_slow(x-2)
+
+d1 = [0] * 100 # 메모이제이션 위한 리스트 초기화. DP 테이블이라고 불림.
+
+def fibo_topdown(x): # 메모이제이션 사용
+    if x == 1 or x == 2:
+        return 1
+    if d1[x] != 0: # 계산한 것이라면 그거 꺼내오기
+        return d1[x]
+    d1[x] = fibo_topdown(x-1) + fibo_topdown(x-2)
+    return d1[x]
+
+def fibo_bottomup(x):
+    d2 = [0] * 100
+    d2[1] = 1
+    d2[2] = 1
+    n = 99
+    for i in range(3, x+1):
+        d2[i] = d2[i-1] + d2[i-2]
+    return d2[x]
+
+print(fibo_slow(10))
+print(fibo_topdown(10))
+print(fibo_bottomup(10))
+    
