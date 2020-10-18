2e7fb46ce2252b96445a3f9d9c8957dc4b602804
KangJunewoo <brianjune.dev@gmail.com>
2020-10-18 18:33:26 +0900

    [ADD] 감시피하기를 풀려던 흔적

--- 3-DFS_BFS/감시피하기.py
+++ 3-DFS_BFS/감시피하기.py
@@ -1,6 +1,50 @@
-n = int(input())
-graph = []
-after_graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
+# BLANK = 'X'
+# OBSTACLE = 'O'
+# TEACHER = 'T'
+# STUDENT = 'S'
+# caught = False
+# n = int(input())
+# graph = []
+# after_graph = []
+# for i in range(n):
+#     graph.append(list(input().split()))
 
+
+# dr = [-1, 0, 1, 0]
+# dc = [0, 1, 0, -1]
+
+# def gamsi_student(r, c):
+#     for i in range(4):
+#         newr = r + dr[i]
+#         newc = c + dc[i]
+#         while 0 <= newr < n and 0 <= newc < n:
+#             if after_graph[newr][newc] == BLANK:
+#                 after_graph[newr][newc] = TEACHER
+#             elif after_graph[newr][newc] == STUDENT:
+#                 return True
+#     return False
+
+# def dfs(on):
+#     global result
+
+#     if on == 3:
+#         for i in range(n):
+#             for j in range(n):
+#                 after_graph[i][j] = graph[i][j]
+#         for i in range(n):
+#             for j in range(n):
+#                 if after_graph[i][j] == TEACHER:
+#                     if gamsi_student(i, j):
+#                         caught = True
+#                         return
+#     for i in range(n):
+#         for j in range(n):
+#             if graph[i][j] == BLANK:
+#                 graph[i][j] = OBSTACLE
+#                 on += 1
+#                 dfs(on)
+#                 graph[i][j] = BLANK
+#                 on -= 1
+
+# dfs(0)
+# print(caught)
