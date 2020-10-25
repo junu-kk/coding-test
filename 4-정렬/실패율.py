98cf7a6d4c44fc8424b5b9b0875ceb16727e0dd1
KangJunewoo <brianjune.dev@gmail.com>
2020-10-25 21:54:45 +0900

    [REFACTOR] 정렬부분 리팩토링

---
+++ 4-정렬/실패율.py
@@ -0,0 +1,16 @@
+def solution(N, stages):
+    fail_rates = []
+    stage_left = len(stages)
+    
+    for i in range(1, N+1): # 1~N스테이지까지 반복 
+        howmany = stages.count(i) # 얼마나 많이 나오는지 센 후
+        if stage_left != 0: # 다 안끝났으면 적용하고
+            fail_rates.append((i, howmany/stage_left))
+        else: # 다끝났으면 실패율 0이고
+            fail_rates.append((i, 0))
+        
+        stage_left -= howmany
+    
+    print(fail_rates)
+    answer = sorted(fail_rates, key=lambda x: x[1], reverse=True) # 실패율 내림차순 정렬.
+    return [i[0] for i in answer]
--- 4-정렬/카드정렬하기.py
+++ 4-정렬/카드정렬하기.py
@@ -8,7 +8,7 @@ for i in range(n):
 
 answer = 0
 
-while len(heap) != 1:
+while len(heap) != 1: # 힙이 한개 남을때까지 두개 pop후 연산후 push
     a = heapq.heappop(heap)
     b = heapq.heappop(heap)
     ab = a + b
