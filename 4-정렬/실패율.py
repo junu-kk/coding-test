11507538ebd3db071b506196e2ed5d9908529430
KangJunewoo <brianjune.dev@gmail.com>
2020-11-15 11:49:46 +0900

    [REFACTOR] 실패율 변수 수정

--- 4-정렬/실패율.py
+++ 4-정렬/실패율.py
@@ -1,15 +1,15 @@
 def solution(N, stages):
     fail_rates = []
-    stage_left = len(stages)
+    people_left = len(stages)
     
     for i in range(1, N+1): # 1~N스테이지까지 반복 
         howmany = stages.count(i) # 얼마나 많이 나오는지 센 후
-        if stage_left != 0: # 다 안끝났으면 적용하고
-            fail_rates.append((i, howmany/stage_left))
+        if people_left != 0: # 다 안끝났으면 적용하고
+            fail_rates.append((i, howmany/people_left))
         else: # 다끝났으면 실패율 0이고
             fail_rates.append((i, 0))
         
-        stage_left -= howmany
+        people_left -= howmany
     
     print(fail_rates)
     answer = sorted(fail_rates, key=lambda x: x[1], reverse=True) # 실패율 내림차순 정렬.
