be945cf88117dc9cdbf9508908a22277285a29c8
KangJunewoo <brianjune.dev@gmail.com>
2020-11-15 12:00:49 +0900

    [ADD] 금광

---
+++ 6-DP/금광.py
@@ -0,0 +1,33 @@
+# DP테이블을 맵 그대로 사용하는 방식.
+# dp[i][j] = arr[i][j] + max(dp[i-1][j-1], dp[i][j-1], dp[i+1][j-1]) -> 리스트 범위 벗어나는지 체크 필요.
+
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split())) # 우선 1차원으로 쭉 입력을 받음.
+
+    # dp테이블 초기화
+    dp = []
+    idx = 0
+    for i in range(n):
+        dp.append(arr[idx:idx+m]) # 그걸 2차원으로 바꿔줌.
+        idx += m
+
+    for j in range(1, m):
+        for i in range(n):
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            if i == n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+            left = dp[i][j-1]
+            dp[i][j] += max(left_up, left_down, left)
+
+        result = 0
+
+        for i in range(n):
+            result = max(result, dp[i][m-1])
+
+        print(result)
