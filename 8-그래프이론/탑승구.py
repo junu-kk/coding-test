819d39c5a53eb42e6232cf4a660818788180d30f
KangJunewoo <brianjune.dev@gmail.com>
2020-12-27 17:13:26 +0900

    [REFACTOR] 탑승구 주석 추가

--- 8-그래프이론/탑승구.py
+++ 8-그래프이론/탑승구.py
@@ -1,9 +1,9 @@
-def find_parent(parent, x):
+def find_parent(parent, x): # 기존 코드와 똑같으니
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
+def union_parent(parent, a, b): # 설명은 생략
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a<b:
@@ -11,8 +11,9 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
-g = int(input())
-p = int(input())
+g = int(input()) # number of gates
+p = int(input()) # number of planes
+
 parent = [0] * (g+1) # 신기하네.. 리스트는 이렇게 선언해도 자동 전역인게.
 
 for i in range(1, g+1):
@@ -25,9 +26,9 @@ for _ in range(p):
 
 for plane in planes:
     data = find_parent(parent, plane)
-    if data == 0:
+    if data == 0: # 0에 다다르면 더 이상 주차할 데가 없다는 뜻.
         break
-    union_parent(parent, data, data-1)
+    union_parent(parent, data, data-1) # data 자리에 주차하면 
     result += 1
 
 print(result)
